17705b12c17e57ccf895c76dd82ee12d124f2218
zhengliru <zhengliru@yaochufa.com>
2018-10-19 17:25:35 +0800

    php特性-匿名函数

--- Reflect.php
+++ Reflect.php
@@ -1,35 +1,67 @@
 <?php
 /**
- * php 反射类的使用
  * Created by PhpStorm.
  * User: zhengliru
  * Date: 2018/10/19
  * Time: 11:56
  */
 
-class A{
+class A
+{
     const C_O_S_T = 'C_VAL';
     public $pubV = 1;
     public $proV = 2;
     public $priV = 3;
-    public function func1(){}
-    protected function func2(){}
-    private function func3(){}
+
+    public function func1($callFunc = '')
+    {
+        if (is_callable($callFunc)) {
+            $callFunc($this);
+        }
+    }
+
+    protected function func2()
+    {
+    }
+
+    private function func3()
+    {
+    }
 }
 
-class Reflect
+class B
+{
+    public function __construct()
+    {
+        echo __CLASS__ . ' - : - ';
+        var_dump(get_called_class());//调用的函数，这里输出 Reflect而不是B
+        echo __CLASS__ . ' - : - ';
+    }
+}
+
+class Reflect extends B
 {
     function __construct()
     {
+        parent::__construct();
         $a = new ReflectionClass('A');
+        echo "ReflectionClass \n";
         var_dump(
             $a,
             $a->getName(),
-            $a->getConstants(),
-            $a->getDefaultProperties(),
-            $a->getMethods()
-            // todo 通过反射获取各种属性 ....
-            );
+//            $a->getConstants(),
+//            $a->getDefaultProperties(),
+//            $a->getMethods(),
+            get_called_class()
+        // todo 通过反射获取各种属性 ....
+        );
+        $aClass = new A();
+        echo "func1 callback \n";
+        $aClass->func1(function ($e) {
+            var_dump($e);
+            echo __CLASS__ . __FUNCTION__ . "\n";
+            echo "----------------- call end \n";
+        });
     }
 
 }
